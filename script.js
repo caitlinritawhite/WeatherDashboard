6ff6a96f94414aa66ea44a8cb989e5980a758eb8
Caitlin White <caitlinritawhite@gmail.com>
2020-08-05 17:45:40 -0400

    Moment JS update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-
+//When the search button is clicked - take the value of the user input, stop default and run weather function 
+//Also clear any input from previous searches
 $("#searchBtn").on("click", function(event){
   var userInput = $("#search-form").val()
   event.preventDefault()
@@ -10,52 +11,60 @@ $("#searchBtn").on("click", function(event){
     $("#current-icon").empty();
 });
 
-
+//Here is our weather function that runs when the search button is clicked
 
 function weather(userInput){
+  //here is our API
     var weatherURL = 
     "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=cb31c15d977ada6c905bc4f191d03d92"
 
-
-localStorage.setItem("city", userInput)
-
-var cityNames = localStorage.getItem(userInput)
-$("#city-storage").text(cityNames)
-
+//running ajax to get our information from our API
     $.ajax({
       url: weatherURL,
       type: "GET"
     }).then(function(response){
 
+//Get the current temperature from the api and convert to farenheit
 var kelvinTemp = response.list[0].main.temp;
 var temperature = (kelvinTemp - 273.15) * 1.80 + 32;
+
+//get the date and display it in the correct format
 var date = moment().format('MM/DD/YYYY')
+
+//get the humidity from the api
 var humidity = response.list[0].main.humidity;
+
+//get the wind speed from the api
 var windSpeed = response.list[0].wind.speed;
 
-
+//get the weather icon from the API
 var weatherIcon = $("<img>");
         weatherIcon.attr("src", "https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
 
-
+//append the information to the page
 $("#current-icon").append(weatherIcon);
 $(".city-name").text(userInput + " (" + date + ")");
 $(".temp").text(temperature.toFixed(1) + "°F");
 $(".humidity").text(humidity + "%");
 $(".wind").text(windSpeed.toFixed(1) + " MPH");
 
+
+//get the latitude and longitude to get the uv index
 var latitude = response.city.coord.lat;
 var longitude =  response.city.coord.lon;
 
+//new API for the uv index information
 var coordinateURL = "http://api.openweathermap.org/data/2.5/uvi?appid=e5f561d692ee5b0d5bfef99cb764f31d&lat=" + latitude + "&lon=" + longitude;
 
-
+//run ajax again to get the information from the new API
 $.ajax({
   url: coordinateURL,
   method: "GET"
 }).then(function (uvIndex) {
+  //get the value of the UV index from the api
 var currentUV = uvIndex.value
 
+//append the uv index to the page and style it
 $(".uv-index").text(currentUV);
 $(".uv-index").css({"background-color": "rgb(243, 57, 88)", "color": "white", "border-radius": "5px", "padding": "3px"})
 
@@ -66,6 +75,7 @@ $(".uv-index").css({"background-color": "rgb(243, 57, 88)", "color": "white", "b
 
 });
 
+//getting information from the API for the 5 day forcast
 var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=cb31c15d977ada6c905bc4f191d03d92"
 
 
@@ -74,6 +84,8 @@ $.ajax({
   method: "GET"
 }).then(function (forecast) {
 
+
+  //for loop - starting at the 2nd index of the array - going through the entire array (40 items) and adding 8 every time (since they are 3 hour intervals)
   for (i=2; i<40; i+=8){
     //new div for 5 day forecast
     var forecastDiv = $('<div>').attr("id", "number" + [i]).addClass('col-2 five-days');
@@ -82,6 +94,7 @@ $.ajax({
     var weekday = forecast.list[i].dt_txt;
     //format the date
     weekday = weekday.slice(0, 10);
+    weekday= moment(weekday).format("MM/DD/YYYY")
     //create html element for the date
         weekday = $('<p>').text(weekday)
 
@@ -99,7 +112,7 @@ $.ajax({
     var weekHumidity = forecast.list[i].main.humidity;
     weekHumidity = $("<p>").text("Humidity: " + weekHumidity)
 
-
+//append the information to the screen
   var final = $(forecastDiv).append(weekday, weekIcon, farWeekTemp, weekHumidity)
         $('#five-day-content').append(final)
 
